390f83fba2b0f1614868132b682b0a799d2199a6
Jose Luis Garcia Marin <josgarmar31@alum.us.es>
2022-11-07 20:54:06 +0100

    FEAT : añadidas las operaciones crud referentes a las direcciones

--- acme_nft/acme_nft_app/urls.py
+++ acme_nft/acme_nft_app/urls.py
@@ -9,4 +9,11 @@ urlpatterns = [
     path("login", views.login, name="login"),
     path("hello/<int:user_id>", views.hello, name="hello"),
     path("error", views.error, name="error"),
+    path("show-address/<int:user_id>", views.show_adrress, name="show-address"),
+    path("new-address", views.new_address, name="new-address"),
+    path("delete-address/<int:address_id>", views.delete_address, name="delete-address"),
+    path("update-address/<int:address_id>", views.update_address, name="update-address"),
+
+
+
 ]
--- acme_nft/acme_nft_app/views.py
+++ acme_nft/acme_nft_app/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from django.urls import reverse
+from .models import User, Product, Address
 
 
 from acme_nft_app.models import User, Product
@@ -76,3 +77,187 @@ def register(request):
             
     else:
         return render(request, "login.html")
+
+def show_adrress(request, user_id):
+
+    user = User.objects.get(id=user_id)
+    list_address = user.address_set.all()
+    print(list_address)
+    return render(request, "show-address.html", context={"list_address": list_address})
+
+def new_address(request):
+
+    if request.method == "POST":
+
+        errors = []
+
+        street_name = request.POST['street_name']
+        number = request.POST['number']
+        if request.POST['floor'] == "":
+            floor = None
+        else:
+            floor = request.POST['floor']
+        if request.POST['block'] == "":
+            block = None
+        else:
+            block = request.POST['block']
+        door = request.POST['door']
+        city = request.POST['city']
+        code_postal = request.POST['code_postal']
+
+        if len(street_name) > 60:
+            street_name_length = "El nombre de la calle no puede tener más de 60 caracteres"
+            errors.append(street_name_length)
+        if len(door) > 1:
+            door_length = "La puerta no puede tener más de 1 caracter"
+            errors.append(door_length)
+        if code_postal.isdigit() == False:
+            code_postal_digit = "El código postal debe ser un número"
+            errors.append(code_postal_digit)
+        else:
+            code_postal = int(code_postal)
+
+        if block.isdigit() == False:
+            block_digit = "El bloque debe ser un número"
+            errors.append(block_digit)
+        else:
+            block = int(block)
+        if floor.isdigit() == False:
+            floor_digit = "El piso debe ser un número"
+            errors.append(floor_digit)
+        else:
+            floor = int(floor)
+        if number.isdigit() == False:
+            number_digit = "El número debe ser un número"
+            errors.append(number_digit)
+        if len(city) > 60:
+            city_length = "El nombre de la ciudad no puede tener más de 60 caracteres"
+            errors.append(city_length)
+        if len(errors) > 0:
+            are_errors = True
+            return render(request, "new-address.html", {
+                "errors": errors,
+                "street_name": street_name,
+                "number": number,
+                "floor": floor,
+                "block": block,
+                "door": door,
+                "city": city,
+                "code_postal": code_postal,
+                "are_errors": are_errors,
+            })
+        else:
+            user = User.objects.get(id=2)
+            address = Address(user_id=user.id, street_name=street_name, number=number, block=block, floor=floor, door=door, city=city, code_postal=code_postal)
+            address.save()
+            return HttpResponseRedirect(reverse("acme-nft:show-address", args=(user.id,)))
+    else:
+        return render(request, "new-address.html")
+
+def delete_address(request, address_id):
+
+    address = Address.objects.get(id=address_id)
+    address.delete()
+    return HttpResponseRedirect(reverse("acme-nft:show-address", args=(2,)))
+
+
+def update_address(request, address_id):
+
+    if request.method == "POST":
+
+        address = Address.objects.get(id=address_id)
+
+        errors = []
+
+        street_name = request.POST['street_name']
+        number = request.POST['number']
+        if request.POST['floor'] == "":
+            floor = None
+        else:
+            floor = request.POST['floor']
+        if request.POST['block'] == "":
+            block = None
+        else:
+            block = request.POST['block']
+        door = request.POST['door']
+        city = request.POST['city']
+        code_postal = request.POST['code_postal']
+
+        if len(street_name) > 60:
+            street_name_length = "El nombre de la calle no puede tener más de 60 caracteres"
+            errors.append(street_name_length)
+        if len(door) > 1:
+            door_length = "La puerta no puede tener más de 1 caracter"
+            errors.append(door_length)
+        if code_postal.isdigit() == False:
+            code_postal_digit = "El código postal debe ser un número"
+            errors.append(code_postal_digit)
+        else:
+            code_postal = int(code_postal)
+
+        if block.isdigit() == False:
+            block_digit = "El bloque debe ser un número"
+            errors.append(block_digit)
+        else:
+            block = int(block)
+        if floor.isdigit() == False:
+            floor_digit = "El piso debe ser un número"
+            errors.append(floor_digit)
+        else:
+            floor = int(floor)
+        if number.isdigit() == False:
+            number_digit = "El número debe ser un número"
+            errors.append(number_digit)
+        if len(city) > 60:
+            city_length = "El nombre de la ciudad no puede tener más de 60 caracteres"
+            errors.append(city_length)
+        if len(errors) > 0:
+            are_errors = True
+            return render(request, "new-address.html", {
+                "errors": errors,
+                "street_name": street_name,
+                "number": number,
+                "floor": floor,
+                "block": block,
+                "door": door,
+                "city": city,
+                "code_postal": code_postal,
+                "are_errors": are_errors,
+            })
+        else:
+            address.street_name = street_name
+            address.number = number
+            address.floor = floor
+            address.block = block
+            address.door = door
+            address.city = city
+            address.code_postal = code_postal
+            address.save()
+            return HttpResponseRedirect(reverse("acme-nft:show-address", args=(2,)))
+    else:
+        address = Address.objects.get(id=address_id)
+        street_name = address.street_name
+        number = address.number
+        floor = address.floor
+        if floor == None:
+            floor = ""
+        block = address.block
+        if block == None:
+            block = ""
+        door = address.door
+        if door == None:
+            door = ""
+        city = address.city
+        code_postal = address.code_postal
+        id = address.id
+
+        return render(request, "update-address.html", {
+                      "street_name": street_name,
+                      "number": number,
+                      "floor": floor,
+                        "block": block,
+                        "door": door,
+                        "city": city,
+                        "code_postal": code_postal,
+                        "id": id,})
+
