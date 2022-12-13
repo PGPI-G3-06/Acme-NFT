296ead792a1a2992056e40c827e8890540b3a976
Alex-GF <yalejandro9@gmail.com>
2022-12-13 11:52:09 +0100

    1.2.2
    Arreglo en docker-settings.py

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -143,9 +143,6 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
         'LOCATION': 'redis://redis:6379/1',
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
     }
 }
 
